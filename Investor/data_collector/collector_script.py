76fdd9d51ffaeb12c980e95669bac9eb7cb7e0d6
Vicenc Rubies Royo <MrRubyRed>
2021-05-07 15:46:49 -0700

    Added TODOs file and Investing.yml env file.

--- Investor/data_collector/collector_script.py
+++ Investor/data_collector/collector_script.py
@@ -1,8 +1,8 @@
 from data_collector_main import WebsiteDataCollector
 
 collector = WebsiteDataCollector(
-				"Fool_collector",
-				"https://www.fool.com/",
-				"https://www.fool.com/investing-news/?page=9999")
+                "Fool_collector",
+                "https://www.fool.com/",
+                "https://www.fool.com/investing-news/?page=9999")
 
-collector.crawl();
+collector.crawl()
